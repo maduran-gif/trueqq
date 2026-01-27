2f5f31c872dadd39c00c7de16c7e937afefc56e5
Manuel Durán Farías <manuelhduran@gmail.com>
2026-01-27 17:48:59 -0500

    Fix API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuración base de axios
 const API = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://trueqq-backend.onrender.com/api'
   headers: {
     'Content-Type': 'application/json',
   },
